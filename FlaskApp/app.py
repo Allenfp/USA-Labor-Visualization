ca2a36967d647bfc446d90a09a8b075606745bf6
Allen Partlow <Allenfp@gmail.com>
2018-03-06 20:39:09 -0800

    Add files via upload

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -21,6 +21,12 @@ def home():
 
     return render_template("index.html")
 
+@app.route("/data")
+def data():
+
+    return render_template("data.html")
+
+
 # Front Page National Information  ################################################################################
 @app.route("/national") 
 def national_info():
